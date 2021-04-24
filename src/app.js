a83a2eb8e474364e49b3fb3082ba103ce5922b06
Renan Gueiros <renangueiros@outlook.com>
2021-04-24 20:53:00 -0300

    Refactor listen from variable to  function

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ const app = express()
 
 const PORT = process.env.PORT || '3000'
 
-const listen = app.listen(PORT, () => {
-  console.log(`Server listening at port ${PORT}`)
-})
+const listen = () => {
+  app.listen(PORT, () => {
+    console.log(`Server listening at port ${PORT}`)
+  })
+}
 
 module.exports.listen = listen
